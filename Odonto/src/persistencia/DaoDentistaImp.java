5a9803052e78416a2893bd7b28cb8ed4062212d8
josue1912 <josue1912@gmail.com>
2012-05-25 22:52:40 +0000

    - DaoDentista - DentistaMB

---
+++ Odonto/src/persistencia/DaoDentistaImp.java
@@ -0,0 +1,99 @@
+package persistencia;
+
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Dentista;
+
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import util.HibernateUtil;
+
+public class DaoDentistaImp implements DaoDentista {
+
+	private Session session = null;
+	private Transaction transaction = null;
+	
+	public boolean cadastrarDentista(Dentista dentista) {
+		boolean retorno = false;
+		try{
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.beginTransaction();
+			session.persist(dentista);
+			transaction.commit();
+			retorno = true;
+		}catch(HibernateException e){
+			transaction.rollback();
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+		return retorno;
+	}
+
+	public boolean alterarDentista(Dentista dentista) {
+		boolean retorno = false;
+		try{
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.beginTransaction();
+			session.update(dentista);
+			transaction.commit();
+			retorno = true;
+		}catch(HibernateException e){
+			transaction.rollback();
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+		return retorno;
+	}
+
+	public boolean excluirDentista(Dentista dentista) {
+		boolean retorno = false;
+		try{
+			session = HibernateUtil.getSessionFactory().openSession();
+			transaction = session.beginTransaction();
+			session.delete(dentista);
+			transaction.commit();
+			retorno = true;
+		}catch(HibernateException e){
+			transaction.rollback();
+			e.printStackTrace();
+		}finally{
+			session.close();
+		}
+		return retorno;
+	}
+
+	public Dentista pesquisarDentista(Dentista dentista) {
+		Dentista e = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			e = (Dentista)session.get(Dentista.class, dentista.getCodigoDentista());
+		} catch (HibernateException ex) {
+			ex.printStackTrace();
+		}finally{
+			session.close();
+		}
+		return e;
+	}
+
+	public List<Dentista> pesquisarTodosDentista() {
+		ArrayList lista = null;
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
+			Criteria cr = session.createCriteria(Dentista.class);
+			lista = (ArrayList)cr.list();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			
+		}finally{
+			session.close();
+			return lista;
+		}
+		
+	}
+
+}
